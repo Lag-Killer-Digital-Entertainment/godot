a278e3a4db577f541cc34f9b8f2c2add529e011c
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2023-11-04 12:43:05 -0300

    Re added the module

---
+++ modules/relativity_engine/observer.h
@@ -0,0 +1,12 @@
+#ifndef RELACTIVITY_ENGINE_MAIN
+#define RELACTIVITY_ENGINE_MAIN
+
+#include "scene/3d/node_3d.h"
+
+
+namespace relactivity_engine {
+class Observer : public Node3D {
+	GDCLASS(Observer, Node3D);
+};
+} //namespace relactivity_engine
+#endif // RELACTIVITY_ENGINE_MAIN
